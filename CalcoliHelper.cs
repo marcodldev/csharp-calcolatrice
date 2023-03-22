b088c1e0bc6f77c5b9b72acc130668cb7858b3ef
Emir-Software <125670961+Emir-Software@users.noreply.github.com>
2023-03-22 14:52:18 +0100

    Aggiungere i file di progetto.

---
+++ CalcoliHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    public static class CalcoliHelper
+    {
+        public static int Somma(int num1, int num2)
+        {
+           int risultato = num1 + num2;
+
+           return risultato;
+        }
+    }
+}

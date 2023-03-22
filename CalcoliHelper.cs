c447a1f4f52236d460d4505a6c0a2567a96c55de
Emir-Software <125670961+Emir-Software@users.noreply.github.com>
2023-03-22 15:02:48 +0100

    + sommaint,sommadouble,difint,difdouble,moltint

--- CalcoliHelper.cs
+++ CalcoliHelper.cs
@@ -6,6 +6,23 @@ using System.Threading.Tasks;
 
 namespace csharp_calcolatrice
 {
+    /*
+     *  V Somma di due numeri interi
+        V Somma di due numeri double
+        V Differenza tra due numeri interi
+        V Differenza tra due numeri double
+        V Moltiplicazione di due numeri interi
+        - Moltiplicazione di due numeri double
+        - Valore assoluto di un numero intero
+        - Valore assoluto di un numero double
+        - Minimo tra due numeri interi
+        - Minimo tra due numeri double
+        - Massimo tra due numeri interi
+        - Massimo tra due numeri double
+     * 
+     * 
+     *
+     */
     public static class CalcoliHelper
     {
         public static int Somma(int num1, int num2)
@@ -13,6 +30,34 @@ namespace csharp_calcolatrice
            int risultato = num1 + num2;
 
            return risultato;
+        }
+
+        public static double SommaDouble(double num1, double num2)
+        {
+            double risultato = num1 + num2;
+
+            return risultato;
+        }
+
+        public static int Differenza(int num1, int num2)
+        {
+            int risultato =  num1 -  num2;
+
+            return risultato;
+        }
+
+        public static double DifferenzaDouble(double num1, double num2)
+        {
+            double risultato = num1 - num2;
+
+            return risultato;
+        }
+
+        public static int Moltiplicazione(int num1, int num2)
+        {
+            int risultato = num1 * num2;
+
+            return risultato;
         }
     }
 }
